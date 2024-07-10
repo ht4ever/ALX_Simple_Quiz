063b59a37328ef243981d2e44059b5ecf5d9d3c0
touati <hishamtouati@gmail.com>
2024-07-10 18:12:59 +0100

    add new folder

---
+++ ALX_Simple_Quiz/quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+    const correctAnswer = "4"; // Value should be a string as radio input values are strings
+    const btn = document.querySelector('#submit-answer');
+    const feedback = document.querySelector('#feedback');
+    
+    btn.addEventListener('click', function() {
+        const userAnswer = document.querySelector('input[name="quiz"]:checked');
+        if (userAnswer && userAnswer.value === correctAnswer) {
+            feedback.textContent = 'Correct! Well done.';
+        } else {
+            feedback.textContent = "That's incorrect. Try again!";
+        }
+    });
+}
+
+checkAnswer();
